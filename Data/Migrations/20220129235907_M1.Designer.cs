dc8f4683a1e1b17a2266abe6d597124431735045
Joonior-Programmer <kkjh6457@gmail.com>
2022-01-29 15:59:31 -0800

    go

--- Data/Migrations/20220129235907_M1.Designer.cs
+++ Data/Migrations/20220129235907_M1.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Code1st.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220129231231_M1")]
+    [Migration("20220129235907_M1")]
     partial class M1
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
